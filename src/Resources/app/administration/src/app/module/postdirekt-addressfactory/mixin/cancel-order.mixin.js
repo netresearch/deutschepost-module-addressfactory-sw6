dad96f9da03cb5fe17ed9f8b11db674a5174b0c5
Paul Siedler <paul.siedler@netresearch.de>
2021-04-26 12:35:05 +0200

    PDSW-100: change cancel functionality to emulate core state switching, to benefit from proper order state updates

--- src/Resources/app/administration/src/app/module/postdirekt-addressfactory/mixin/cancel-order.mixin.js
+++ src/Resources/app/administration/src/app/module/postdirekt-addressfactory/mixin/cancel-order.mixin.js
@@ -10,24 +10,8 @@ Mixin.register('postdirekt.addressfactory.cancel-order', {
         Mixin.getByName('notification'),
     ],
     methods: {
-        cancelOrder(order) {
-            this.isLoading = true;
-
-            this.orderStateMachineService.transitionOrderState(
-                order.id,
-                'cancel'
-            ).then(ApiService.handleResponse
-            ).then((state) => {
-                order.stateMachineState = state;
-                this.$emit('order-state-change');
-            }).catch((error) => {
-                this.createNotificationError({
-                    title: this.$tc('global.default.error'),
-                    message: this.$tc('sw-order.stateCard.labelErrorStateChange') + error
-                });
-            }).finally(() => {
-                this.isLoading = false;
-            });
+        cancelOrder() {
+            this.$emit('order-state-change', 'cancel');
         }
     }
 });
