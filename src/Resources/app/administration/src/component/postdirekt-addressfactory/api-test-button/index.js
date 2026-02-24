fd547c252377816c1b54c19cb72f69067ba87abc
Carl Monk <carl@winkelwagen.de>
2026-02-24 17:21:17 +0100

    PDSW-201: Restored 'Test API Access' button functionality

--- src/Resources/app/administration/src/component/postdirekt-addressfactory/api-test-button/index.js
+++ src/Resources/app/administration/src/component/postdirekt-addressfactory/api-test-button/index.js
@@ -16,12 +16,32 @@ Shopware.Component.register('postdirekt-addressfactory-api-test-button', {
         };
     },
     methods: {
+        getSystemConfig() {
+            let p = this.$parent;
+            while (p) {
+                if (p.$options?.name === 'sw-system-config') {
+                    return p;
+                }
+                p = p.$parent;
+            }
+            return null;
+        },
+        getConfigValue(configData, key) {
+            return (configData[null] && configData[null][key]) || configData[key];
+        },
         onButtonClick() {
+            const sysConfig = this.getSystemConfig();
+            if (!sysConfig) {
+                console.error('Failed retrieving config field values. Make sure the button\'s nested inside a sw-system-config component.');
+                return;
+            }
+
             this.isLoading = true;
-            const username = document.querySelector("[id='NRLEJPostDirektAddressfactory.config.apiUser']").value;
-            const password = document.querySelector("[id='NRLEJPostDirektAddressfactory.config.apiPassword']").value;
-            const configurationName = document.querySelector("[id='NRLEJPostDirektAddressfactory.config.configurationName']").value;
-            const clientId = document.querySelector("[id='NRLEJPostDirektAddressfactory.config.clientId']").value;
+
+            const username = this.getConfigValue(sysConfig.actualConfigData, 'NRLEJPostDirektAddressfactory.config.apiUser');
+            const password = this.getConfigValue(sysConfig.actualConfigData, 'NRLEJPostDirektAddressfactory.config.apiPassword');
+            const configurationName = this.getConfigValue(sysConfig.actualConfigData, 'NRLEJPostDirektAddressfactory.config.configurationName');
+            const clientId = this.getConfigValue(sysConfig.actualConfigData, 'NRLEJPostDirektAddressfactory.config.clientId');
 
             return this.postdirektTestCredentialService.testCredentials(username, password, configurationName, clientId)
                 .then((response) => {
